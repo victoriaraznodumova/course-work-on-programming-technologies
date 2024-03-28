eb1514af8a36fa8107f423f14fdd7488815fb44d
Виктория Разнодумова <raznodumova@gmail.com>
2024-03-29 00:17:06 +0300

    first commit

---
+++ src/main/java/com/example/new_kursach_by_me/NewKursachByMeApplication.java
@@ -0,0 +1,13 @@
+package com.example.new_kursach_by_me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewKursachByMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewKursachByMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/new_kursach_by_me/NewKursachByMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.new_kursach_by_me;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewKursachByMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
